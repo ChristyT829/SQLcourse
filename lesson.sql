54cc6e81af02ab3fa021eab289e2ab5d030d817c
ChristyT829 <christy.thurlow@nhs.net>
2026-09-11 13:02:46 +0000

    second commit

--- lesson.sql
+++ lesson.sql
@@ -1,8 +1,17 @@
 /*
 Example lesson stub
 */
+SELECT ps.patientid,
+       ps.hospital,
+       ps.ward,
+       ps.admitteddate,
+       ps.dischargedate,
+       ps.ethnicity
+FROM   PatientStay AS ps
+WHERE  ps.hospital IN ('kingston', 'pruh')
+       AND (ps.ward LIKE '%surgery'
+            OR ps.ward LIKE '%o%')
+       AND ps.ethnicity IS NULL;
 
-SELECT
-	*
-FROM
-	PatientStay ps ;
+
+--select distinct hospital from PatientStay ps
